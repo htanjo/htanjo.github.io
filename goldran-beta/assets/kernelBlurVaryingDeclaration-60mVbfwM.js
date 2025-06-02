636b867937c563690d81552007445ef48ac068ff
Hiroyuki Tanjo <expheno@gmail.com>
2025-06-02 16:56:19 +0900

    Remove goldran-beta

--- goldran-beta/assets/kernelBlurVaryingDeclaration-60mVbfwM.js
+++
@@ -1,1 +0,0 @@
-import{S as r}from"./Screen-hPtYg6u8.js";const e="kernelBlurVaryingDeclaration",a="varying sampleCoord{X}: vec2f;";r.IncludesShadersStoreWGSL[e]=a;
--- goldran-beta/assets/kernelBlurVaryingDeclaration-DrH98Dty.js
+++
@@ -1,1 +0,0 @@
-import{S as r}from"./Screen-hPtYg6u8.js";const e="kernelBlurVaryingDeclaration",a="varying vec2 sampleCoord{X};";r.IncludesShadersStore[e]=a;
